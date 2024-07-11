9fcef3c62744a3fe2f05256584f579debc80f428
FIEF-Nohell <noel.hmnn@gmail.com>
2024-07-11 09:23:25 +0200

    🚀feat(history-display) use guessNumber as key for mapping history items

--- src/app/components/history-display.tsx
+++ src/app/components/history-display.tsx
@@ -7,7 +7,7 @@ export default function HistoryDisplay({ history }: HistoryDisplayProps) {
             <div className="flex space-x-4">
                 {history.slice(0, 3).map((item, index) => (
                     <motion.div
-                        key={index}
+                        key={item.guessNumber}
                         className="guess-square flex flex-col items-center justify-center transition-transform duration-200"
                         initial={{ opacity: 0.7, y: 50 }}
                         animate={{ opacity: 1, y: 0 }}
