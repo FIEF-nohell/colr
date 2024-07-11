6ab6ebc743cccc9a8dbf0cb0a584e8b9ab553044
FIEF-Nohell <noel.hmnn@gmail.com>
2024-07-11 12:59:28 +0200

    🛠️refactor(workflows) remove export step from GitHub Actions 🛠️refactor(config) fix formatting in next config file ⚙️chore(scripts) remove unused export script from package.json

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ export default {
     output: 'export',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/colr/' : '',
     images: {
-      unoptimized: true,
+        unoptimized: true,
     },
-  };
-  
+};
